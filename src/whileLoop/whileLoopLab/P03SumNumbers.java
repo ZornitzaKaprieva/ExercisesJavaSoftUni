a1564c9981f4e25997bdc654cac13a469d551085
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-02-06 11:31:48 +0200

    Java Practice from SoftUni: While Loop Lab P03

---
+++ src/whileLoop/whileLoopLab/P03SumNumbers.java
@@ -0,0 +1,25 @@
+package whileLoop.whileLoopLab;
+
+import java.util.Scanner;
+
+/*Напишете програма, която чете цяло число от конзолата
+и на всеки следващ ред цели числа,
+докато тяхната сума стане по-голяма или равна на първоначалното число.
+След приключване на четенето да се отпечата сумата на въведените числа.*/
+public class P03SumNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+
+        while (sum < number) {
+            int nextNumber = Integer.parseInt(scanner.nextLine());
+            sum += nextNumber;
+        }
+
+            System.out.println(sum);
+        }
+    }
