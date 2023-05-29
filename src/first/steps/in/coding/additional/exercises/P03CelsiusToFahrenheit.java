ce3dce6ac031d4efb890af8d05ef86a75516b1aa
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-05-29 08:48:53 +0300

    Java Practice from SoftUni: Additinal Exercises: First Steps in Coding (P03)

--- src/first/steps/in/coding/additional/exercises/P03CelsiusToFahrenheit.java
+++ src/first/steps/in/coding/additional/exercises/P03CelsiusToFahrenheit.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 /*Напишете програма, която чете градуси по скалата на Целзий (°C)
 и ги преобразува до градуси по скалата на Фаренхайт (°F).
 Потърсете в Интернет подходяща формула, с която да извършите изчисленията.
-Форматирате изхода до втория знак след десетичната запетая.
- */
+Форматирате изхода до втория знак след десетичната запетая.*/
+
 public class P03CelsiusToFahrenheit {
 
     public static void main(String[] args) {
@@ -18,7 +18,6 @@ public class P03CelsiusToFahrenheit {
         double fahrenheit = celsius * 1.8 + 32;
 
         System.out.printf("%.2f", fahrenheit);
-
     }
 }
 
