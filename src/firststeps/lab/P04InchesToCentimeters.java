41de473bd95dc3d130953eb1ebc5f3945e395f91
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-05-30 09:13:05 +0300

    P04

--- src/firststeps/lab/P04InchesToCentimeters.java
+++ src/firststeps/lab/P04InchesToCentimeters.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 /*Да се напише програма, която чете от конзолата реално число и го преобразува от инчове в сантиметри.
 За целта умножете инчовете по 2.54 (1 инч = 2.54 сантиметра).*/
+
 public class P04InchesToCentimeters {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         double inch = Double.parseDouble(scanner.nextLine());
         double cm = inch * 2.54d;
 
         System.out.println(cm);
-
     }
 }
