52eb8ba9602b65856cf1134a4cf4174660340bd9
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-05-30 09:16:06 +0300

    P06

--- src/firststeps/lab/P06ConcatenateData.java
+++ src/firststeps/lab/P06ConcatenateData.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 /*Напишете програма, която прочита от конзолата име, фамилия, възраст и град и печата следното съобщение:
 "You are <firstName> <lastName>, a <age>-years old person from <town>."*/
+
 public class P06ConcatenateData {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         String name = scanner.nextLine();
@@ -14,6 +16,5 @@ public class P06ConcatenateData {
         String town = scanner.nextLine();
 
         System.out.printf("You are %s %s, a %d-years old person from %s.", name, surname, age, town);
-
     }
 }
