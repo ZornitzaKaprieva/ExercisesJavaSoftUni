86daabe19d113edcda38673cdc3b29c998f731bd
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-05-31 08:42:24 +0300

    P03

--- src/firststeps/lab/P03RectangleArea.java
+++ src/firststeps/lab/P03RectangleArea.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class P03RectangleArea {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         int a = Integer.parseInt(scanner.nextLine());
