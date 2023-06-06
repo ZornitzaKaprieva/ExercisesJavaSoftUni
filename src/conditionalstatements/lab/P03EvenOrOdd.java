b72a850e014c2ca49b9159b99c988208d65dd8ef
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-06 08:42:39 +0300

    P03

--- src/conditionalstatements/lab/P03EvenOrOdd.java
+++ src/conditionalstatements/lab/P03EvenOrOdd.java
@@ -3,12 +3,13 @@ package conditionalstatements.lab;
 import java.util.Scanner;
 
 /*Да се напише програма, която чете цяло число, въведено от потребителя и печата дали е четно или нечетно.
-Ако е четно отпечатайте "even", ако е нечетно "odd".
-*/
-public class P03EvenOrOdd {
+Ако е четно отпечатайте "even", ако е нечетно "odd".*/
 
+public class P03EvenOrOdd {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+        
         int isOddOrEven = Integer.parseInt(scanner.nextLine());
 
         if(isOddOrEven % 2 == 0) { //модулно деление (hint: 1548 % 10 = 8 (последната цифра); 1548 % 100 = 48 (последните 2 цифри)
