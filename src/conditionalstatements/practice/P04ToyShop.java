572ef029a0dda1abb1bdf14c1d01050b020e4227
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-07 13:02:10 +0300

    P04

--- src/conditionalstatements/practice/P04ToyShop.java
+++ src/conditionalstatements/practice/P04ToyShop.java
@@ -11,23 +11,21 @@ package conditionalstatements.practice;
 Ако поръчаните играчки са 50 или повече магазинът прави отстъпка 25% от общата цена.
 От спечелените пари Петя трябва да даде 10% за наема на магазина.
 Да се пресметне дали парите ще ѝ стигнат да отиде на екскурзия.
-
 Изход
 На конзолата се отпечатва:
 •	Ако парите са достатъчни се отпечатва:
 o	"Yes! {оставащите пари} lv left."
 •	Ако парите НЕ са достатъчни се отпечатва:
 o	"Not enough money! {недостигащите пари} lv needed."
-
-Резултатът трябва да се форматира до втория знак след десетичната запетая.
-
-*/
+Резултатът трябва да се форматира до втория знак след десетичната запетая.*/
 
 import java.util.Scanner;
 
 public class P04ToyShop {
-    public static void main(String[] args) {
+       public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         double trip = Double.parseDouble(scanner.nextLine());
         int puzzleNum = Integer.parseInt(scanner.nextLine());
         int dollNum = Integer.parseInt(scanner.nextLine());
@@ -42,21 +40,20 @@ public class P04ToyShop {
         double minionPrice = minionNum * 8.20;
         double truckPrice = truckNum * 2.00;
         double sumPrice = puzzlePrice + dollPrice + bearPrice + minionPrice + truckPrice;
-            if (numPurchase >= 50) {
-                 sumPrice = sumPrice - (sumPrice * 0.25);
-             }
+        if (numPurchase >= 50) {
+            sumPrice = sumPrice - (sumPrice * 0.25);
+        }
 
         double profitWithRent = sumPrice - (sumPrice * 0.1);
         double enoughMoney = profitWithRent - trip;
         double neededMoney =  trip - profitWithRent;
 
-
-                if (trip <= profitWithRent) {
-                    System.out.printf("Yes! %.2f lv left.", enoughMoney);
-                } else {
-                    System.out.printf("Not enough money! %.2f lv needed.", neededMoney);
-                }
+        if (trip <= profitWithRent) {
+            System.out.printf("Yes! %.2f lv left.", enoughMoney);
+        } else {
+            System.out.printf("Not enough money! %.2f lv needed.", neededMoney);
         }
     }
+}
 
 
