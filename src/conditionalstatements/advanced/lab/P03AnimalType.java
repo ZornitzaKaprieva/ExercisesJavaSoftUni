a7a2f4d476a0eaa4c4a42ec225a7d03aed6d7faa
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-09 08:52:22 +0300

    P03

--- src/conditionalstatements/advanced/lab/P03AnimalType.java
+++ src/conditionalstatements/advanced/lab/P03AnimalType.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 /*Напишете програма, която отпечатва класа на животното според неговото име, въведено от потребителя.
 •	dog -> mammal
 •	crocodile, tortoise, snake -> reptile
-•	others -> unknown
-*/
+•	others -> unknown*/
+
 public class P03AnimalType {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         String animal = scanner.nextLine();
