10594f3bb0e9d57815745f70f4f17bccabdc8f51
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-09 08:55:30 +0300

    P04

--- src/conditionalstatements/advanced/lab/P04PersonalTitles.java
+++ src/conditionalstatements/advanced/lab/P04PersonalTitles.java
@@ -1,27 +1,24 @@
 package conditionalstatements.advanced.lab;
 
-
 import java.util.Scanner;
 
-/*Да се напише конзолна програма,
-която прочита възраст (десетично число) и пол ("m" или "f"), въведени от потребителя
-и отпечатва обръщение измежду следните:
+/*Да се напише конзолна програма, която прочита възраст (десетично число) и пол ("m" или "f"), 
+въведени от потребителя и отпечатва обръщение измежду следните:
 •	"Mr." - мъж (пол "m") на 16 или повече години
 •	"Master" - момче (пол "m") под 16 години
-
 •	"Ms." - жена (пол "f") на 16 или повече години
-•	"Miss" - момиче (пол "f") под 16 години
-*/
+•	"Miss" - момиче (пол "f") под 16 години*/
+
 public class P04PersonalTitles {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         float age = Float.parseFloat(scanner.nextLine());
         String sex = scanner.nextLine();
 
         //solution1:
-
         switch (sex) {
             case "m":
                 if (age >= 16) {
@@ -40,19 +37,18 @@ public class P04PersonalTitles {
         }
 
         //solution 2:
-
-        if (sex.equals("m")) {
-            if (age >= 16) {
-                System.out.println("Mr.");
-            } else {
-                System.out.println("Master");
-            }
-        } else if (sex.equals("f")){
-            if (age >= 16) {
-                System.out.println("Ms.");
-            } else {
-                System.out.println("Miss");
-            }
-        }
+//         if (sex.equals("m")) {
+//             if (age >= 16) {
+//                 System.out.println("Mr.");
+//             } else {
+//                 System.out.println("Master");
+//             }
+//         } else if (sex.equals("f")){
+//             if (age >= 16) {
+//                 System.out.println("Ms.");
+//             } else {
+//                 System.out.println("Miss");
+//             }
+//         }
     }
 }
