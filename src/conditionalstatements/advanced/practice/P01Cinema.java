abbd47940cb8827913fa74e9b31f857fdb51eadf
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-13 10:03:33 +0300

    P01

--- src/conditionalstatements/advanced/practice/P01Cinema.java
+++ src/conditionalstatements/advanced/practice/P01Cinema.java
@@ -10,10 +10,11 @@ import java.util.Scanner;
 Напишете програма,
 която чете тип прожекция (стринг), брой редове и брой колони в залата (цели числа), въведени от потребителя,
 и изчислява общите приходи от билети при пълна зала.
-Резултатът да се отпечата във формат като в примерите по-долу, с 2 знака след десетичната точка.
-*/
+Резултатът да се отпечата във формат като в примерите по-долу, с 2 знака след десетичната точка.*/
+
 public class P01Cinema {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         String projectionType = scanner.nextLine();
@@ -31,10 +32,6 @@ public class P01Cinema {
         }
 
         double totalIncome = ticketPrice * (numberOfRows * numberOfColumns);
-
         System.out.printf("%.2f leva", totalIncome);
-
-
-
     }
 }
