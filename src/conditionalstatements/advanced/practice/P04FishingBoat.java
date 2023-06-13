1ddacca186990f710b21ff184315c2f1a449106f
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-13 10:09:55 +0300

    P04

--- src/conditionalstatements/advanced/practice/P04FishingBoat.java
+++ src/conditionalstatements/advanced/practice/P04FishingBoat.java
@@ -2,8 +2,7 @@ package conditionalstatements.advanced.practice;
 
 import java.util.Scanner;
 
-/*Тони и приятели много обичали да ходят за риба,
-те са толкова запалени по риболова, че решават да отидат на риболов с кораб.
+/*Тони и приятели много обичали да ходят за риба, те са толкова запалени по риболова, че решават да отидат на риболов с кораб.
 Цената за наема на кораба зависи от сезона и броя рибари.
 Цената зависи от сезона:
 •	Цената за наем на кораба през пролетта е  3000 лв.
@@ -26,11 +25,11 @@ import java.util.Scanner;
 "Yes! You have {останалите пари} leva left."
 •	Ако бюджетът НЕ Е достатъчен:
 "Not enough money! You need {сумата, която не достига} leva."
-Сумите трябва да са форматирани с точност до два знака след десетичната запетая.
-Примерен вход и изход
-*/
+Сумите трябва да са форматирани с точност до два знака след десетичната запетая.*/
+
 public class P04FishingBoat {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         int groupBudget = Integer.parseInt(scanner.nextLine()); //[1…8000]
@@ -43,10 +42,8 @@ public class P04FishingBoat {
         double groupDiscount = 0;
 
         switch (season){
-
             case "Spring":
                 seasonRent = 3000;
-                
                     if (numberOfFishermen <= 6) {
                         groupDiscount = seasonRent * 0.1;
                     } else if (numberOfFishermen > 6 && numberOfFishermen <= 11) {
@@ -55,11 +52,9 @@ public class P04FishingBoat {
                         groupDiscount = seasonRent * 0.25;
                     }
                 break;
-
             case "Summer":
             case "Autumn":
                 seasonRent = 4200;
-
                 if (numberOfFishermen <= 6) {
                     groupDiscount = seasonRent * 0.1;
                 } else if (numberOfFishermen > 6 && numberOfFishermen <= 11) {
@@ -68,10 +63,8 @@ public class P04FishingBoat {
                     groupDiscount = seasonRent * 0.25;
                 }
                 break;
-
             case "Winter":
                 seasonRent = 2600;
-
                 if (numberOfFishermen <= 6) {
                     groupDiscount = seasonRent * 0.1;
                 } else if (numberOfFishermen > 6 && numberOfFishermen <= 11) {
@@ -83,7 +76,6 @@ public class P04FishingBoat {
         }
 
         double rentalPrice = seasonRent - groupDiscount;
-
         if (numberOfFishermen % 2 == 0 && !season.equals("Autumn")) {
             rentalPrice = rentalPrice * 0.95;
         }
