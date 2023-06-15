f27002e67da5e48142fb737d6ce67fd6cfe50abf
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-15 09:05:31 +0300

    P10

--- src/loops/forloop/lab/P10OddEvenSum.java
+++ src/loops/forloop/lab/P10OddEvenSum.java
@@ -6,18 +6,17 @@ import java.util.Scanner;
 и проверява дали сумата от числата на четни позиции е равна на сумата на числата на нечетни позиции.
 •	Ако сумите са равни да се отпечатат два реда: "Yes" и на нов ред "Sum = " + сумата;
 •	Ако сумите не са равни да се отпечат два реда: "No" и на нов ред "Diff = " + разликата.
-Разликата се изчислява по абсолютна стойност.
-*/
+Разликата се изчислява по абсолютна стойност.*/
 
 public class P10OddEvenSum {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
 
         int evenSum = 0;
         int oddSum = 0;
-
         for (int i = 0; i < n; i++) {
             int currentNum = Integer.parseInt(scanner.nextLine());
             //отделям  четни от нечетни:
@@ -26,7 +25,6 @@ public class P10OddEvenSum {
             } else {
                 oddSum += currentNum;
             }
-
         }
 
         int diff = Math.abs(evenSum-oddSum);
