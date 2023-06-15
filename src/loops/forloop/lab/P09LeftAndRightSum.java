c458e9fedaf9ecb0d1cd19080de558ed5d3c37e6
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-15 09:04:35 +0300

    P09

--- src/loops/forloop/lab/P09LeftAndRightSum.java
+++ src/loops/forloop/lab/P09LeftAndRightSum.java
@@ -6,24 +6,26 @@ import java.util.Scanner;
 и проверява дали сумата на първите n числа (лява сума) е равна на сумата на вторите n числа (дясна сума).
 При равенство печата " Yes, sum = " + сумата; иначе печата " No, diff = " + разликата.
 Разликата се изчислява като положително число (по абсолютна стойност). */
+
 public class P09LeftAndRightSum {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         //solution 1:
-        int n = Integer.parseInt(scanner.nextLine()); //1. задаваме колко числа ще приема програмата.
+        int n = Integer.parseInt(scanner.nextLine()); 
 
         //for цикъл за първата сума:
         int leftSum = 0;
-        for (int i = 1; i <= n; i++) { //щом не е стринг е ок да по ва от 1 (зависи от логиката)
-            int currentNum = Integer.parseInt(scanner.nextLine()); //2. Въвеждаме толкова числа, колкото ни иска програмата
+        for (int i = 1; i <= n; i++) { //щом не е стринг е ок да почва от 1 (зависи от логиката)
+            int currentNum = Integer.parseInt(scanner.nextLine()); 
             leftSum = leftSum + currentNum;
-            }
+        }
 
         //for цикъл за втората сума:
         int rightSum = 0;
-        for (int i = 1; i <= n; i++) { //щом не е стринг е ок да по ва от 1 (зависи от логиката)
-            int currentNum = Integer.parseInt(scanner.nextLine()); //2. Въвеждаме толкова числа, колкото ни иска програмата
+        for (int i = 1; i <= n; i++) { 
+            int currentNum = Integer.parseInt(scanner.nextLine());
             rightSum = rightSum + currentNum;
         }
 
@@ -35,7 +37,6 @@ public class P09LeftAndRightSum {
         }
 
         //solution 2:
-
         /*int n2 = Integer.parseInt(scanner.nextLine()); //1. задаваме колко числа ще приема програмата.
 
         int leftSum2 = 0;
@@ -48,9 +49,7 @@ public class P09LeftAndRightSum {
                 leftSum2 = leftSum2 + currentNum2;
             } else {
                 rightSum2 = rightSum2 + currentNum2;
-
             }
-
         }
         if (leftSum2 == rightSum2) {
             System.out.printf("Yes, sum = %d%n", leftSum2);
