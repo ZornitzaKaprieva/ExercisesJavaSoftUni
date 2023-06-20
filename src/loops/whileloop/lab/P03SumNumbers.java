22d545e46c219a35b5cb34b484a93649e0951af9
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-20 08:52:09 +0300

    P03

--- src/loops/whileloop/lab/P03SumNumbers.java
+++ src/loops/whileloop/lab/P03SumNumbers.java
@@ -2,10 +2,10 @@ package loops.whileloop.lab;
 
 import java.util.Scanner;
 
-/*Напишете програма, която чете цяло число от конзолата
-и на всеки следващ ред цели числа,
+/*Напишете програма, която чете цяло число от конзолата и на всеки следващ ред цели числа,
 докато тяхната сума стане по-голяма или равна на първоначалното число.
 След приключване на четенето да се отпечата сумата на въведените числа.*/
+
 public class P03SumNumbers {
     public static void main(String[] args) {
 
@@ -14,12 +14,11 @@ public class P03SumNumbers {
         int number = Integer.parseInt(scanner.nextLine());
 
         int sum = 0;
-
         while (sum < number) {
             int nextNumber = Integer.parseInt(scanner.nextLine());
             sum += nextNumber;
         }
-
+        
             System.out.println(sum);
-        }
     }
+}
