318ecffa903c58e30fb30387d7ed2151410287f5
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-20 08:51:17 +0300

    P02

--- src/loops/whileloop/lab/P02Password.java
+++ src/loops/whileloop/lab/P02Password.java
@@ -2,20 +2,17 @@ package loops.whileloop.lab;
 
 import java.util.Scanner;
 
-//https://pastebin.com/u/veronikaaa86
-
 /*Напишете програма, която първоначално прочита име и парола на потребителски профил.
 След това чете парола за вход.
 •	при въвеждане на грешна парола: потребителя да се подкани да въведе нова парола.
-•	при въвеждане на правилна парола: отпечатваме "Welcome {username}!".
-*/
+•	при въвеждане на правилна парола: отпечатваме "Welcome {username}!".*/
+
 public class P02Password {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        //solution 1:
         String name = scanner.nextLine();
         String password = scanner.nextLine();
         String confirmPass = scanner.nextLine();
@@ -25,6 +22,5 @@ public class P02Password {
         }
 
         System.out.printf("Welcome %s!\n", name);
-
-        }
     }
+}
