e27a521f09eb27486740cad587b9c89ed5a86d66
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-21 14:52:58 +0300

    P06

--- src/loops/whileloop/lab/P06MaxNumber.java
+++ src/loops/whileloop/lab/P06MaxNumber.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 /*Напишете програма, която до получаване на командата "Stop",
 чете цели числа, въведени от потребителя и намира най-голямото измежду тях.
 Въвежда се по едно число на ред.*/
+
 public class P06MaxNumber {
     public static void main(String[] args) {
 
@@ -12,17 +13,16 @@ public class P06MaxNumber {
 
         String input = scanner.nextLine();
 
-        int maxNum = Integer.MIN_VALUE; //сравняваме го с вазможно най-малкото число. в края ще изелезе най-голямото число
+        int maxNum = Integer.MIN_VALUE; //сравняваме го с вазможно най-малкото число, за да намерим най-голямото число
 
         while (!input.equals("Stop")){
             int currentNumber = Integer.parseInt(input);
             input = scanner.nextLine();
 
-            if (currentNumber > maxNum) { //правим си проверка: ако новото въведено число е по-голямо от maxNum, значи ече то е най-голямо число
+            if (currentNumber > maxNum) { //правим си проверка: ако новото въведено число е по-голямо от maxNum, значи вече то е най-голямо число
                 maxNum = currentNumber;
             }
         }
-
         System.out.println(maxNum);
     }
 }
