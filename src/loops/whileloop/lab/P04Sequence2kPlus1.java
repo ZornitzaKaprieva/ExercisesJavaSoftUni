b21045347ddfdddd12f1a47b1e4941347665c622
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-21 14:49:18 +0300

    P04

--- src/loops/whileloop/lab/P04Sequence2kPlus1.java
+++ src/loops/whileloop/lab/P04Sequence2kPlus1.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 /*Напишете програма, която чете число n, въведено от потребителя,
 и отпечатва всички числа ≤ n от редицата: 1, 3, 7, 15, 31, ….
 Всяко следващо число се изчислява като умножим предишното с 2 и добавим 1.*/
-public class P04Sequence2kPlus1 {
 
+public class P04Sequence2kPlus1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
