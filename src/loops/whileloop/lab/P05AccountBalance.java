199c0c4e5365405ce34daf1d40e73f974b89152e
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-21 14:52:05 +0300

    P05

--- src/loops/whileloop/lab/P05AccountBalance.java
+++ src/loops/whileloop/lab/P05AccountBalance.java
@@ -7,22 +7,20 @@ import java.util.Scanner;
 При всяка получена сума на конзолата трябва да се извежда "Increase: " + сумата и тя да се прибавя в сметката.
 Ако получите число по-малко от 0 на конзолата трябва да се изведе "Invalid operation!"  и програмата да приключи.
 Когато програмата приключи трябва да се принтира "Total: " + общата сума в сметката форматирана до втория знак след десетичната запетая. */
+
 public class P05AccountBalance {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine(); //5.5; NoMoreMoney
-
+        String input = scanner.nextLine(); 
+        
         double sum = 0;
-
         while (!input.equals("NoMoreMoney")) {
-
-            double amount = Double.parseDouble(input); // така преобразуваме стринг на дабъл
-
+            double amount = Double.parseDouble(input); // String to double
             if (amount < 0) {
                 System.out.println("Invalid operation!\n");
-                break; // дотук прекратявам, ако е отрицателно число
+                break; // if it is a negative number, while loop breaks
             }
 
             sum = sum + amount; // събираме въведените числа
@@ -30,7 +28,6 @@ public class P05AccountBalance {
             System.out.printf("Increase: %.2f\n", amount); //сумата, която се прибавя в сметката.
             input = scanner.nextLine();  //продължаваме да въвеждаме сума, докато не се въведе NoMoreMoney
         }
-
         System.out.printf("Total: %.2f\n", sum); //принтираме сумата
     }
 }
