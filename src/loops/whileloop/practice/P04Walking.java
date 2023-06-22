302a1510d54a0e22ca918d2ec9c48090f1341a3b
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-06-22 10:46:16 +0300

    P04

--- src/loops/whileloop/practice/P04Walking.java
+++ src/loops/whileloop/practice/P04Walking.java
@@ -11,26 +11,24 @@ import java.util.Scanner;
 Ако иска да се прибере преди това, тя ще въведе командата "Going home"
 и ще въведе стъпките, които е извървяла докато се прибира.
 След което, ако не е успяла да постигне целта си, на конзолата трябва да се изпише:
-"{разликата между стъпките} more steps to reach goal."
-*/
+"{разликата между стъпките} more steps to reach goal."*/
+
 public class P04Walking {
 
     public static void main(String[] args) {
-
 
         Scanner scanner = new Scanner(System.in);
 
         int stepsGoal = 10000;
         String stepsDay = scanner.nextLine(); //"Going home" //1. прочитаме входните данни
+        
         int countSteps = 0;//4. да видим колко стъпки е изминала (в while цикъла)
-
         while (!stepsDay.equals("Going home")) { // 2.добавяме while за stepsDay
             int currentSteps = Integer.parseInt(stepsDay); //преобразуваме стъпките от Стринг в инт, за да можем да ги сметмем след това
 
             countSteps += currentSteps;
-
-
-            if (countSteps >= stepsGoal) { //може и да принтираме тук (соуф), но в случая няма смисъл, защото ще дублираме с принта в края.
+            
+            if (countSteps >= stepsGoal) { //може и да принтираме тук, но в случая няма смисъл, защото ще дублираме с принта в края.
                 break;
             }
 
@@ -43,16 +41,12 @@ public class P04Walking {
         }
 
         //дори да не е събрала 10000 стъпки преди going home, може да ги събере, докато стигне. затова слагаме следващия if..else
-
         if (countSteps < stepsGoal) {
             int leftStepsForDay = stepsGoal - countSteps;
             System.out.printf("%d more steps to reach goal.", leftStepsForDay);
         } else {
             int stepsOverTheGoal = countSteps - stepsGoal;
             System.out.printf("Goal reached! Good job!\n%d steps over the goal!\n", stepsOverTheGoal);
-
         }
-
-
     }
 }
