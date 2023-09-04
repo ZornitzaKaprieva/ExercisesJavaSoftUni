d35e995fa8fd20408d92b57e1744b8ac0e8543c9
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-09-04 11:28:57 +0300

    P02

---
+++ src/loops/nestedloops/exercises/P02LettersCombinations.java
@@ -0,0 +1,43 @@
+package loops.nestedloops.exercises;
+
+import java.util.Scanner;
+
+/*Напишете програма, която да принтира на конзолата всички комбинации от 3 букви в зададен интервал,
+като се пропускат комбинациите съдържащи зададена от конзолата буква. Накрая трябва да се изпринтира броят на отпечатаните комбинации.
+Вход
+Входът се чете от конзолата и съдържа точно 3 реда:
+Ред 1.	Малка буква от английската азбука за начало на интервала – от ‘a’ до ‚z’.
+Ред 2.	Малка буква от английската азбука за край на интервала  – от първата буква до ‚z’.
+Ред 3.	Малка буква от английската азбука – от ‘a’ до ‚z’ – като комбинациите съдържащи тази буквата се пропускат.
+Изход
+Да се отпечатат на един ред всички комбинации отговарящи на условието плюс броят им разделени с интервал.*/
+
+public class P02LettersCombinations {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        char start = scanner.next().charAt(0);
+        char end = scanner.next().charAt(0);
+        char omitted = scanner.next().charAt(0);
+        int count = 0;
+
+        String combinations = "";
+        for (char i = start; i <= end; i++) {
+            if (i != omitted){
+                for (char j = start; j <= end; j++) {
+                    if (j != omitted){
+                        for (char k = start; k <= end; k++) {
+                            if (k != omitted){
+                                count++;
+                                combinations +=  "" + i + j + k + " ";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.printf("%s%d", combinations, count);
+    }
+}
