a77efa4df157b8b8479c48a40c5b778ef320fc5a
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-09-04 11:23:18 +0300

    P09

---
+++ src/conditionalstatements/advanced/exercises/P09NumbersFrom1to10.java
@@ -0,0 +1,13 @@
+package conditionalstatements.advanced.exercises;
+
+//Напишете програма, която отпечатва числата от 1 до 10, по едно на ред.
+
+public class P09NumbersFrom1to10 {
+
+    public static void main(String[] args) {
+
+        for (int a = 1; a <= 10; a++){
+            System.out.println(a);
+        }
+    }
+}
