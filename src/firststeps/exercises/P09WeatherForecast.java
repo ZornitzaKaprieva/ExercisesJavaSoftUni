72c87ea9ebf9e94eb06b3afd7ea210c78a978b1f
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-11-07 09:18:03 +0200

    P09

--- src/firststeps/exercises/P09WeatherForecast.java
+++ src/firststeps/exercises/P09WeatherForecast.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 /*Напишете програма, която познава дали е топло или студено навън.
 От конзолата се чете един ред – текст, който подсказва какво е времето.
 При въвеждане на "sunny" трябва да се отпечата "It's warm outside!".
-Във всички останали случаи трябва да се отпечата "It's cold outside!".*/
+Във всички останали случаи трябва да се отпечата "It's cold outside!".
+Примерен вход1: sunny
+Примерен изход1: It's warm outside!
+Примерен вход2: snowy
+Примерен изход2: It's cold outside!*/
 
 public class P09WeatherForecast {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
