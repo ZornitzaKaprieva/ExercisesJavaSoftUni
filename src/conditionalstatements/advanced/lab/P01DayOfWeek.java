f05b7b4d34d7d9d28b0ac661a3aa597ed28f934e
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-11-09 12:15:56 +0200

    P01

--- src/conditionalstatements/advanced/lab/P01DayOfWeek.java
+++ src/conditionalstatements/advanced/lab/P01DayOfWeek.java
@@ -3,10 +3,18 @@ package conditionalstatements.advanced.lab;
 import java.util.Scanner;
 
 /*Напишете програма, която чете цяло число, въведено от потребителя, и отпечатва ден от седмицата (на английски език), 
-в граници [1...7] или отпечатва "Error" в случай, че въведеното число е невалидно. */
+в граници [1...7] или отпечатва "Error" в случай, че въведеното число е невалидно.
+Примерен вход и изход:
+1	Monday
+2	Tuesday
+3	Wednesday
+4	Thursday
+5	Friday
+6	Saturday
+7	Sunday
+-1	Error*/
 
 public class P01DayOfWeek {
-
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
