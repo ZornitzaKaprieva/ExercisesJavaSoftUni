ca8fab926df856588f49cc18404e462c2f548542
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-11-14 08:42:28 +0200

    P01

--- src/conditionalstatements/exercises/P01PipesInPool.java
+++ src/conditionalstatements/exercises/P01PipesInPool.java
@@ -15,7 +15,11 @@ import java.util.Scanner;
 •	До колко се е запълнил басейна и коя тръба с колко процента е допринесла.
 o	"The pool is {запълненост на басейна в проценти}% full. Pipe 1: {процент вода от първата тръба}%. Pipe 2: {процент вода от втората тръба}%."
 Aко басейнът се е препълнил – с колко литра е прелял за даденото време.
-o	"For {часовете, които тръбите са пълнили вода} hours the pool overflows with {литрите вода в повече} liters."*/
+o	"For {часовете, които тръбите са пълнили вода} hours the pool overflows with {литрите вода в повече} liters."
+Примерен вход1: 1000  100  120  3
+Примерен изход1: The pool is 66.00% full. Pipe 1: 45.45%. Pipe 2: 54.55%.
+Примерен вход2: 100  100  100  2.5
+Примерен изход2: For 2.50 hours the pool overflows with 400.00 liters.*/
 
 public class P01PipesInPool {
     public static void main(String[] args) {
