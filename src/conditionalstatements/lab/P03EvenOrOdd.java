12358c844eb7a8ee4f41c011a8d332c0dbd10b1a
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-11-17 14:09:18 +0200

    P03

--- src/conditionalstatements/lab/P03EvenOrOdd.java
+++ src/conditionalstatements/lab/P03EvenOrOdd.java
@@ -3,7 +3,11 @@ package conditionalstatements.lab;
 import java.util.Scanner;
 
 /*Да се напише програма, която чете цяло число, въведено от потребителя и печата дали е четно или нечетно.
-Ако е четно отпечатайте "even", ако е нечетно "odd".*/
+Ако е четно отпечатайте "even", ако е нечетно "odd".
+Примерен вход1: 25
+Примерен изход1: odd
+Примерен вход2: 1024
+Примерен изход2: even*/
 
 public class P03EvenOrOdd {
     public static void main(String[] args) {
