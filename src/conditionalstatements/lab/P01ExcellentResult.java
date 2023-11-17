eb939ca5f0b496e7786b19600392fbc9f1e944ec
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-11-17 14:07:50 +0200

    P01

--- src/conditionalstatements/lab/P01ExcellentResult.java
+++ src/conditionalstatements/lab/P01ExcellentResult.java
@@ -3,7 +3,13 @@ package conditionalstatements.lab;
 import java.util.Scanner;
 
 /*Напишете програма, която чете оценка (цяло число), въведена от потребителя
-и отпечатва "Excellent!", ако оценката е 5 или по-висока.*/
+и отпечатва "Excellent!", ако оценката е 5 или по-висока.
+Примерен вход1: 6
+Примерен изход1: Excellent!
+Примерен вход2: 5
+Примерен изход2: Excellent!
+Примерен вход3: 4
+Примерен изход3: (няма изход)*/
 
 public class P01ExcellentResult {
     public static void main(String[] args) {
